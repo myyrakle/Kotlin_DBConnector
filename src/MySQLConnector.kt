edb467179d14f84eb8545ac072f1e700d5c7dbc0
myyrakle <sssang97@naver.com>
2019-12-27 18:25:58 +0900

    Update MySQLConnector.kt

--- src/MySQLConnector.kt
+++ src/MySQLConnector.kt
@@ -2,7 +2,7 @@ import java.sql.Connection
 import java.sql.DriverManager
 import java.sql.SQLException
 
-class MySQLConnector : IDBConnector {
+class MySQLConnector : SQLDBConnector {
     override var connection: Connection? = null
         private set
 
